aa598867bd7deaaded1a6003ccf67b3b605c5fe9
misxiaoxiao <1102820661@qq.com>
2021-11-23 12:28:13 +0800

    feat 项目名称和版本号合法性校验

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -3,9 +3,13 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const fse = require('fs-extra');
+const semver = require('semver');
 
 const Command = require('@cli-dev-test/command');
 const log = require('@cli-dev-test/log');
+
+const TYPE_PROJECT = 'project';
+const TYPE_COMPONENT = 'component';
 
 class InitCommand extends Command {
   init () {
@@ -59,8 +63,76 @@ class InitCommand extends Command {
         }
       }
     }
-    // 3. 选择创建项目或组件
-    // 4. 获取项目的基本信息
+    return this.getProjectInfo()
+  }
+
+  async getProjectInfo () {
+    const projectInfo = {};
+    // 1. 选择创建项目或组件
+    const { type } = await inquirer.prompt({
+      type: 'list',
+      name: 'type',
+      message: '请选择初始化类型',
+      default: TYPE_PROJECT,
+      choices: [
+        { name: '项目', value: TYPE_PROJECT },
+        { name: '组件', value: TYPE_COMPONENT }
+      ]
+    });
+    log.verbose('type', type)
+    if (type === TYPE_PROJECT) {
+      // 2. 获取项目的基本信息
+      const o = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'projectName',
+          message: '请输入项目名称',
+          default: '',
+          // 校验规则
+          // 1. 输入的首字符必须为英文字符
+          // 2. 尾字符必须为英文或数字，不能为字符
+          // 3. 字符仅允许"-_"
+          // 合法: a, a-b, a_b, a-b-c, a_b_c, a-b1-c1, a_b1_c1, a1, a1-b1-c1, a1_b1_c1
+          // 不合法: 1, a_, a-, a_1, a-1
+          validate: function (v) {
+            const done = this.async();
+
+            setTimeout(() => {
+              if (!/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)) {
+                done('请输入合法的项目名称');
+                return;
+              }
+              done(null, true);
+            }, 0);
+            // return /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)
+          },
+          filter: v => v
+        },
+        {
+          type: 'input',
+          name: 'projectVersion',
+          message: '请输入项目版本号',
+          default: '1.0.0',
+          validate: function (v) {
+            const done = this.async();
+            
+            setTimeout(() => {
+              if (!semver.valid(v)) {
+                done('请输入合法的项目版本号');
+                return;
+              }
+              done(null, true);
+            }, 0);
+          },
+          filter: v => !!semver.valid(v) ? semver.valid(v) : v
+        }
+      ])
+      console.log(o)
+    } else if (type === TYPE_COMPONENT) {
+
+    }
+    // return 项目基本信息（object）
+    return projectInfo;
   }
 
   isDirEmpty (localPath) {
